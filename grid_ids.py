0183664650773d4c44f0a7f81646886a27fb0152
Ivo Georgiev <ivogeorg@gmail.com>
2023-06-19 21:39:17 +0000

    grid IDS

--- grid_ids.py
+++ grid_ids.py
@@ -23,34 +23,33 @@ grid = np.array([
 
 print("Grid shape: ", grid.shape)
 
-# Offset multipliers relative to a node position (considered (0, 0))
-multipliers = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
-
 rand_pos = (randint(0, grid.shape[0]), randint(0, grid.shape[1]))
 print("Random grid position: ", rand_pos)
 
 if not grid[rand_pos[0], rand_pos[1]]:
-    print("Position {1} is clear".format(rand_pos))   
+    print("Position {} is clear".format(rand_pos))   
 else:
+    print("Position {} is obstructed".format(rand_pos))   
     found = False
     # Iterative deepening in a square of side equal to the smaller grid 
     # dimension
     for r in range(1, min(grid.shape[0], grid.shape[1])):
         # Generate the list of node offsets
-        multipliers = []
-        for n in range(-r, r + 1):
-            for e in range(-r, r + 1):
-                multipliers.append((n, e))
+        offsets = [(n, e) for e in range(-r, r + 1) for n in range(-r, r + 1)]
         # Try all adjacent nodes at "radius" r
-        for m in multipliers:
-            # Check out of bounds
-            new_pos = (rand_pos[0] + r * m[0], rand_pos[1] + r * m[1])
+        for m in offsets:
+            # Adjacent position
+            new_pos = (rand_pos[0] + m[0], rand_pos[1] + m[1])
+			# Check if within the grid
             if new_pos[0] >= 0 and new_pos[0] < grid.shape[0] and \
                 new_pos[1] >= 0 and new_pos[1] < grid.shape[1]:
+				# Check if clear
                 if not grid[new_pos[0], new_pos[1]]:
-                    print("Found clear position ({1}, {2})".format( \
+                    print("Found clear position ({0}, {1})".format( \
                         new_pos[0], new_pos[1]))
-                found = True
-                break
+                    found = True
+                    break
         if found:
+            print("Search succeeded")
             break
+
