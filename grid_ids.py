4aef74f7ac3a377794639d64a70fd700615d777a
Ivo Georgiev <ivogeorg@gmail.com>
2023-06-19 11:12:34 -0600

    Multiplier array has to be dynamic

--- grid_ids.py
+++ grid_ids.py
@@ -33,10 +33,19 @@ if not grid[rand_pos[0], rand_pos[1]]:
     print("Position {1} is clear".format(rand_pos))   
 else:
     found = False
-    for i in range(min(grid.shape[0], grid.shape[1])):
+    # Iterative deepening in a square of side equal to the smaller grid 
+    # dimension
+    for i in range(min(grid.shape[0], grid.shape[1])): # TODO: Start at 1
+        # Circle around
+        # TODO: This doesn't circle around at all (diag and i > 1)
         for m in multipliers:
-            if not grid[i * m[0], i * m[1]]:
-                print("Found clear position ({1}, {2})".format(i * m[0], i * m[1]))
+            # Check out of bounds
+            new_pos = (rand_pos[0] + i * m[0], rand_pos[1] + i * m[1])
+            if new_pos[0] >= 0 and new_pos[0] < grid.shape[0] and \
+                new_pos[1] >= 0 and new_pos[1] < grid.shape[1]:
+                if not grid[new_pos[0], new_pos[1]]:
+                    print("Found clear position ({1}, {2})".format( \
+                        new_pos[0], new_pos[1]))
                 found = True
                 break
         if found:
