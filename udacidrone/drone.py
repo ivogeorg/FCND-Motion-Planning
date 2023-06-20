c8c98534cb757d42e418049924a79e95c1797d66
Ivo Georgiev <ivogeorg@gmail.com>
2023-06-19 21:31:55 -0600

    Handling CONNECTION_CLOSED first

--- udacidrone/drone.py
+++ udacidrone/drone.py
@@ -113,14 +113,15 @@ class Drone(object):
 
         def on_message_receive(msg_name, msg):
             """Sorts incoming messages, updates the drone state variables and runs callbacks"""
-            print('Message received', msg_name, msg)
+            # print('Message received', msg_name, msg)
+            if msg_name == MsgID.CONNECTION_CLOSED:
+                self.stop()
+
             if (((msg.time - self._message_time) > 0.0)):
                 self._message_frequency = 1.0 / (msg.time - self._message_time)
                 self._message_time = msg.time
                 self._time_bias = msg.time - time.time()
 
-            if msg_name == MsgID.CONNECTION_CLOSED:
-                self.stop()
             if msg_name in self._update_property.keys():
                 self._update_property[msg_name](msg)
             self.notify_callbacks(msg_name, msg)  # pass it along to these listeners
